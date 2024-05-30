8fce30d9445c59abc6f729fdb52371722ea41233
Karthik  S <karthiksaravanan.s@noblq.com>
2024-05-30 16:03:26 +0530

    variables

---
+++ Variables,Expressions & statement/Variables/Example1.py
@@ -0,0 +1,7 @@
+# A variable is a name that refers to a value. It is a name for a value for future use.
+# a = 1, here a -> variable : 1 -> value : = -> assignment statement.
+# so writing the name (equals to "=") the value is called assignment statement, which creates a new variable and give them value.
+# the equal sign "=" used here is not the same as the equal sign that is used in maths, "=" is the assignment statement which means that it assigns this value to this variable over here.
+# in future if we call this variable, it give the value againt that variable.
+
+karthik = 9667099704
