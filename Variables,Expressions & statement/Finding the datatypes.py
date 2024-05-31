f05be67ccbc486db16522f15988021fa11b0faa0
Karthik1364 <158262594+Karthik1364@users.noreply.github.com>
2024-05-31 16:46:59 +0530

    changes in explanations

--- Variables,Expressions & statement/Finding the datatypes.py
+++ Variables,Expressions & statement/Finding the datatypes.py
@@ -1,5 +1,5 @@
 # finding the datatypes.
-# we can also find the datatype in the console by typing "type(1)".
+# we can also find the datatype in the console by typing "type()".
 # in python to find the datatype we need to put the type inside the "print function".
 
 print(type("Hello world"))
--- Variables,Expressions & statement/Value and Types/Interger/boolean.py
+++ Variables,Expressions & statement/Value and Types/Interger/boolean.py
@@ -1,6 +1,6 @@
 # boolean.
 # it only has two possible values which is "True" or "False".
-# note: in python we have to write in the (capital letters) i.e "Ture" & "False", in some other languages it doesn't matter but in python it matters.
+# note: in python we have to write the first letter in the (capital letters) i.e "Ture" & "False", in some other languages it doesn't matter but in python it matters.
 # and they don't have any quotes around them.
 
 True 
