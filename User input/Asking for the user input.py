0237521a4f5f7e681d8a038a8ead8dfb064c6d20
Karthik1364 <158262594+Karthik1364@users.noreply.github.com>
2024-06-03 16:40:50 +0530

    changes

--- User input/Asking for the user input.py
+++ User input/Asking for the user input.py
@@ -1,10 +1,10 @@
-# asing for the user input in python.
+# asking for the user input in python.
 
 # sometimes we would like to take the value for a variable from the user via their keyboard.
 # for this purpose python provides a built-in function called input, so this function gets input from the user or from the keyboard.
 
 # Ex:
-print("what's your name?") # here it prints the string inside the print function, but here no way the user to be able to insert the data over here which be used in our program.
+print("what's your name?") # here it prints the string inside the print function, but here no way the user to be able to insert the data over here, which be used in our program.
 
 # here istead of print function we use different function called "input function".
 input("what is your name?")
@@ -12,11 +12,11 @@ input("what is your name?")
 # Ex:2
 print("hello " + input("what is your name?")) # here is we have print function and inside the print function we have called input function in this case, which is nested function.
 
-# whenever we run this code first the input function part will be executed first,
+# whenever we run this code the input function part will be executed first,
 # after giving the input then it will print hello with the given input inside the print function.
 
 # Ex: 3
-name = input("what is your name?\n") # here "\n" represents a new line in the console.
+name = input("what is your name?\n") # here "\n" represents a new line break in the console.
 print("hello " + name)
 
 # note: "\n" which is special character that uses a line break.
