2e786e1e9f5ade4496f82dc4914abd4f1ee3dba1
Karthik1364 <158262594+Karthik1364@users.noreply.github.com>
2024-06-03 12:27:16 +0530

    compeleted operators

---
+++ Variables,Expressions & statement/Operators in python/operators for string.py
@@ -0,0 +1,41 @@
+# operators in string
+
+# "+" we can use plus operator for the string.
+# "*" we can also use multiplication operator for the string.
+
+#### using "+" sign:
+
+# Ex: 1
+print("Hello" + "Karthik") # in python we call it as concatenation.
+
+# if you want to concatenate two string we can use "+" symbol.
+
+#### using "*" symbol:
+
+# Ex: 2
+#print("Hello" * "Karthik") # here we cannot use "*" symbol for two strings.
+
+# Ex: 3 
+print("Hello " * 4) #we can use space, it is concatenating Hello three times.
+
+
+### While we are working with operators, the most common word that we are using is expression.
+
+## What is an Expression?
+
+# An expression is a combination of values, variables, and operators. A value all by itself is considered an expression, and so in a variable, so the following are all legal expressions.
+
+# 20
+# x
+# x + 20
+
+# Ex:
+name = "karthik" # it is called an expression.
+print(name *4)
+
+#### so as the beginner you need to know whenever someone asks expression in python? 
+###### everything relates to variables, operators and values are called expressions.
+
+
+
+
--- Variables,Expressions & statement/Operators in python/operators.py
+++ Variables,Expressions & statement/Operators in python/operators.py
@@ -24,5 +24,26 @@ print(5 ** 2) # by writing two asterisks "**" we can raise the number to the pow
 ## so by using this pemdas rule, we can identify the priority of the opertions in python as we have in mathematics.
 ## note: here multiplication and division are equally important, PEMD||AS, here PEMD is the priority and AS is secondary.
 
-# Ex:
+# Ex: 1
 print(2*2+2/2-2)
+
+# Ex: 2
+print(11 // 3) # "//" operator is used to get the quotient.
+print(11 % 3) # (modulus %) operator is used to get the remainder.
+
+# note: there might be situation that you want to check whether one number is divisible by another
+# so if the result from modulus operator is zero, it means that the first number is divisible by second number.
+
+# Ex: 3
+print(12 % 3) # it will return zero.
+
+# note: so by calling the modulus operator, we can identify if any number is divisible by the second number or not.
+
+# Ex: 4
+print(12 % 10) # if I put 10 instead of 3, you will see that it is going to extract the second number form this two digit number.
+
+# if you want to extract the one digits you can put 10.
+# if you want to extract both digits you can put 100.
+
+# Ex: 4
+print(12 % 100)
