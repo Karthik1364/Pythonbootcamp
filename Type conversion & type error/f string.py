56e1043c56a2a8cc4f6b426f7b7537e8fb8a914e
Karthik1364 <158262594+Karthik1364@users.noreply.github.com>
2024-06-07 15:11:40 +0530

    f string

---
+++ Type conversion & type error/f string.py
@@ -0,0 +1,15 @@
+# strings and rounding numbers.
+
+# so instead of doing all these conversions and concatenations over here, we can use "F sting".
+# "F string" allows us to combine all different data types together without converting and concatenating.
+# note: in front of string we just need to type "F".
+
+year = input('Enter number of years ')
+weeks = 52 * int(year)
+# print('There are ' + str(weeks), 'weeks in ' + year, 'years')
+
+# F string:
+print(f'There are {weeks} weeks in {year} years')
+
+# note: here the f should be placed before the double or single quotes.
+# note: and we are putting the variables (that needs to be converted) inside curly braces over here.
