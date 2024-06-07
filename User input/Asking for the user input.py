9fde6178ddc7e4106229d7f7653b87f0d59c7fbf
Karthik1364 <158262594+Karthik1364@users.noreply.github.com>
2024-06-07 10:40:25 +0530

    changes in user input

--- User input/Asking for the user input.py
+++ User input/Asking for the user input.py
@@ -3,13 +3,13 @@
 # sometimes we would like to take the value for a variable from the user via their keyboard.
 # for this purpose python provides a built-in function called input, so this function gets input from the user or from the keyboard.
 
-# Ex:
+# Ex: 1
 print("what's your name?") # here it prints the string inside the print function, but here no way the user to be able to insert the data over here, which be used in our program.
 
 # here istead of print function we use different function called "input function".
 input("what is your name?")
 
-# Ex:2
+# Ex: 2
 print("hello " + input("what is your name?")) # here is we have print function and inside the print function we have called input function in this case, which is nested function.
 
 # whenever we run this code the input function part will be executed first,
