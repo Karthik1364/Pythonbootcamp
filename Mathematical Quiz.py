c5f083f8a41b3ab1c14f583b33acd4cf064b255d
Karthik1364 <158262594+Karthik1364@users.noreply.github.com>
2024-06-07 15:53:29 +0530

    round function

---
+++ Mathematical Quiz.py
@@ -0,0 +1,25 @@
+# Excercise: 1
+# you are a computer, what will this line of code print?
+
+print(5 + 4 / 2 - (1 + 3))
+
+# steps:
+# 5 + 4 / 2 - (1 + 3)
+# 5 + 2.0 - (1 + 3)
+# 7.0 - (1 + 3)
+# 7.0 - (4)
+# 3.0
+
+# Excercise: 2
+
+a = int(6.5) / int("2")
+print(a)
+
+# Excercise: 3
+
+name = input("what is your name? ")
+print(f'hi {name}')
+
+age = 18
+print(f'you are {age} years old')
+
---
+++ Type conversion & type error/Round function.py
@@ -0,0 +1,21 @@
+# Round function in python.
+
+# whenever we divide a number by another number, we always get floating number.
+# it doesn't matter if this number is integer or float, but the result from the division will be float.
+
+# Ex: 1
+print(10/3)
+
+# Ex: 2
+print(10//3) # "//" is called float division used to ignore the decimal nummbers.
+
+# Ex: 3
+# if there is 3.5, we take it as 4, in that way we can use round function to round the decimal numbers.
+print(round(10/3))
+
+# Ex: 4
+print(round(10/3,2)) # in this case we can bring the required decimal number by using (comma).
+
+# Ex: 5 
+print(round(3.53344555,3)) # not only in division but by providing the float number we can cut short the decimal number using round function.
+
