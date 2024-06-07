6534bda124477884e2f3409a9578b9073283be66
Karthik1364 <158262594+Karthik1364@users.noreply.github.com>
2024-06-07 12:05:47 +0530

    comments in python

---
+++ Comments in python.py
@@ -0,0 +1,26 @@
+# comments in python.
+
+# what is comments in python?
+
+# Basically this kind of notes, we are adding to our course to remember things very easily.
+# as programs get bigger and more complicated, they grt more difficult to understand.
+# so whenever we write a complex program, when we come back to our code in the future, if you have any problem, so it is very difficult to understand what is our code doing.
+# so fot those cases, python comments are very useful.
+# basically if you want to write a comment in python, it starts with "#" symbol.
+
+# The name variable takes name from console.
+name = input("What is your name? ")
+print("Hello" + name)
+
+# three reasons for using comments in our code:
+# 1 : you can use the comments to explain the codes itself.
+# 2 : we can make our code more readable.
+# 3 : when you are testing the code you might want not to execute one part of code.
+
+# note: by using the comment "#" symbol we can make a code as comment before or after execution.
+
+# Ex: 2
+
+a = 10*50/100
+# b = 100
+print(b)
