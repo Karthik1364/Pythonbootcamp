dd2fa73db22f60aa6db9945211c54adc95f83575
Karthik1364 <158262594+Karthik1364@users.noreply.github.com>
2024-06-07 14:54:56 +0530

    type conversion

---
+++ Type conversion & type error/coding ex1.py
@@ -0,0 +1,24 @@
+# Data Types - Weeks in Years
+# Write a program that converts number of years to the weeks. if the input was 2, then the output should be 2 * 52 = 104, because we have 52 weeks in 1 year.
+
+# Example Input
+
+# 2
+
+# Example Output
+
+# 2 * 52 = 104
+
+# There are 104 weeks in 2 years
+
+
+# Hint:
+
+# The prompt for input function has to be as shown below, otherwise the test case will not work.
+'Enter number of years'
+
+years = int(input('Enter number of years '))
+weeks = years * 52
+con1 = str(weeks)
+con2 = str(years)
+print('There are ' + con1, 'weeks in ' + con2, 'years')
---
+++ Type conversion & type error/coding ex2.py
@@ -0,0 +1,3 @@
+year = input('Enter number of years ')
+weeks = 52 * int(year)
+print('There are ' + str(weeks), 'weeks in ' + year, 'years')
