4efd57cc7502e09e32bb0247fdb8e72c96291f52
Karthik1364 <158262594+Karthik1364@users.noreply.github.com>
2024-06-10 16:47:03 +0530

    changes

---
+++ Conditionl Execution/Boolean expression.py
@@ -0,0 +1,28 @@
+# Boolean expression
+
+# The python boolean type has only two possible values
+# 1. True
+# 2. False
+
+# Boolean expressions are very crucial when it comes to conditional execution.
+
+# If you want to check an expression, whether it's true or false, we are using operators.
+# Now the first operator is "equal operator" which is "equal sign" (==) from math.
+
+# "==" equal:
+# so if you want to check that if two vaules are equal, we need to use equal sign over here.
+# here you need to take into account that equal sign is different from the equal sign that we use in maths, i.e "=" this single sign we use it in the maths.
+# But here in python, the equal sign is denoted by double equal "==" sign over here.
+# for example. if I want to check an expression like this.
+
+# Ex: 1
+print(1 == 1) # in this case it returns True.
+
+# note: single equal "=" sign in python is used as assignment operator.
+# note: if you use "==" sign over here, it's becoming an expression.
+
+# Ex: 2
+print(1==2) # in this case it returns False.
+
+
+
