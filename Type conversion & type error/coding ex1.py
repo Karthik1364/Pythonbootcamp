f141456def1d26c653986a3312ad8a346529b3a5
Karthik1364 <158262594+Karthik1364@users.noreply.github.com>
2024-06-10 13:14:13 +0530

    changes

--- Type conversion & type error/coding ex1.py
+++ Type conversion & type error/coding ex1.py
@@ -1,11 +1,11 @@
 # Data Types - Weeks in Years
 # Write a program that converts number of years to the weeks. if the input was 2, then the output should be 2 * 52 = 104, because we have 52 weeks in 1 year.
 
-# Example Input
+# Example Input:
 
 # 2
 
-# Example Output
+# Example Output:
 
 # 2 * 52 = 104
 
