64bcc197c7d7813f1059c7704aa3506fba977136
Karthik1364 <158262594+Karthik1364@users.noreply.github.com>
2024-06-12 17:38:33 +0530

    nested if

--- Conditionl Execution/Nested Condition.py
+++ Conditionl Execution/Nested Condition.py
@@ -1,2 +1,17 @@
 # Nested Condition:
 
+# means one condition can be also nested within another condition.
+
+# example for the IF & ESLE:
+
+# if condition: # if this first condition is True,
+#   if another condition: # we are continuing to another if condition over here.
+#       do this     # if this condition is ok will execute this.
+#   else:
+#       do this  # if not this condition will be executed inside the if condition.
+# else:         
+#   do this   # if the above if inside if condition is not correct, we will process with the outside else part.
+
+# note: once the first condition has passed, we can check for another condition and then we can have another if/esle statement inside this if condition over here.
+
+
