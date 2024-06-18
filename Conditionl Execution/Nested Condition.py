ee20b58cd72ec2a3d9402d1a8f9a8092c294ca40
Karthik1364 <158262594+Karthik1364@users.noreply.github.com>
2024-06-18 10:33:18 +0530

    nested if condition

--- Conditionl Execution/Nested Condition.py
+++ Conditionl Execution/Nested Condition.py
@@ -14,3 +14,24 @@
 
 # note: once the first condition has passed, we can check for another condition and then we can have another if/esle statement inside this if condition over here.
 
+### Example: 1
+# in this condition we are checking if x is equal to y or not.
+# if it is equal to y, we are printing out x and y are equal.
+# if they are not equal, we are printing out they are not equal.
+x = 1
+y = 3
+if x == y:
+    print("x and y are equal")
+else:
+    print("x and y are not equal")
+
+# let's say if we have another condition, which we need to check that if x is greater than y or not. so in this case we need nested if condition.
+# so we are going to write nested if condition inside else statement over here.
+# so if they are not equal, we are going to check another of condition if x greater than y or not.
+if x == y: # here if the x and y are equal, it prints that x and y are equal.
+    print("x and y are equal")
+else:  # here if the x and y are not equal, the else part will be executed.
+    if x > y:
+        print("x greater than y")
+    else:
+        print("x less than y")
