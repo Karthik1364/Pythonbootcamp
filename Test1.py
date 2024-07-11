9d14f44e5b3c3a4b3ece74e4c3e37a7d6c4584d9
Karthik1364 <158262594+Karthik1364@users.noreply.github.com>
2024-07-11 15:49:53 +0530

    changes

--- Test1.py
+++ Test1.py
@@ -1,8 +1,18 @@
 def getMax(n1, n2):
+    # return the biggest number between n1 & n2
     if n1 < n2:
         return n2
     else:
         return n1
     
 print(getMax(5,7))
+
+
+def getMax(n1, n2):
+    # return the biggest number between n1 & n2
+    if 5 < 7:
+        return 7
+    else:
+        return 5
     
+print(getMax(5,7))
