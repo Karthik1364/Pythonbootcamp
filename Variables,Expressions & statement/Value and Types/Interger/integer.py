8b588906c00851c705668635100ab341c639505b
Karthik1364 <158262594+Karthik1364@users.noreply.github.com>
2024-07-18 16:54:18 +0530

    changes

--- Variables,Expressions & statement/Value and Types/Interger/integer.py
+++ Variables,Expressions & statement/Value and Types/Interger/integer.py
@@ -1,4 +1,4 @@
-# interger.
+# integer.
 # these are numbers without any decimal places.
 # integer won't come with single are double quotes.
 # in python if we need to perform any mathematical calculation, the number must be without the double quotes.
